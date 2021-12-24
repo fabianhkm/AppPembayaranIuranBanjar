4fd64cf217bdc8400937d1fdb9133d356d84c3f1
fabianhkm <fabianhakim@student.unud.ac.id>
2021-12-24 10:49:25 +0800

    App Pembayaran Iuran Banjar - 1905551032

---
+++ app/src/main/java/id/fabian/pembayaraniuranbanjar/home.java
@@ -0,0 +1,51 @@
+package id.fabian.pembayaraniuranbanjar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        TextView addlist = findViewById(R.id.addpenduduk);
+        TextView ceklist = findViewById(R.id.listpenduduk);
+        ImageView addlistpng = findViewById(R.id.addpng);
+        Button QuitBtn = findViewById(R.id.quitbtn);
+
+        addlist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pindahactivity = new Intent(home.this,MainActivity.class);
+                startActivity(pindahactivity);
+            }
+        });
+
+        ceklist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pindahAct = new Intent(home.this,listmenu.class);
+                startActivity(pindahAct);
+            }
+        });
+
+
+        QuitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishAffinity();
+            }
+        });
+
+
+    }
+
+}
